731dbb9e5cf2b764e5ccec9639aab9da49ac24ba
Pierantonio Merlino <pierantonio.merlino@eurotech.com>
2026-01-15 10:15:03 +0100

    ci: Update jenkins groovy file with new config (#4)
    * Update jenkins groovy file with new config
    
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>
    
    * Added tests folder to exclusions
    
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>
    
    ---------
    
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- .jenkins/ci.groovy
+++ .jenkins/ci.groovy
@@ -7,7 +7,8 @@ node {
             enable: false,
             projectKey: "eclipse-kura_kura-opcua",
             tokenId: "sonarcloud-token-kura-opcua",
-            exclusions: "tests/**/*.java"
+            exclusions: "tests/**/*,**/*.xml,**/*.yml",
+            testExclusions: "**/*"
         ],
     )
 }
